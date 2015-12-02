0cf69cf0db6874fb5f911928ca7d4b0aacde2bd9
Marina <marina@linuxmint.linuxmint-domain>
2015-12-02 23:50:55 +0400

    added reading from input file

---
+++ main.py
@@ -0,0 +1,48 @@
+from xml.dom.minidom import parse
+
+def getEdge(value):
+    return int(value.value) - 1
+
+def calcResistance(a, b):
+    return 1.0*(a*b)/(a+b)
+
+
+input = parse("input.xml")
+weights = []
+
+ids = input.getElementsByTagName('net')
+for i in xrange(len(ids)):
+    weights.append([0]*len(ids))
+
+resists = input.getElementsByTagName('resistor')
+for resistor in resists:
+    currentValue = weights[getEdge(resistor.attributes['net_from'])][getEdge(resistor.attributes['net_to'])]
+    reversed_currentValue = weights[getEdge(resistor.attributes['net_to'])][getEdge(resistor.attributes['net_from'])]
+    value = float(resistor.attributes['resistance'].value)
+    valueToAssign = value if currentValue == 0 else calcResistance(currentValue, value)
+    reversed_valueToAssign = value if reversed_currentValue == 0 else calcResistance(reversed_currentValue, value)
+    weights[getEdge(resistor.attributes['net_from'])][getEdge(resistor.attributes['net_to'])] = valueToAssign
+    weights[getEdge(resistor.attributes['net_to'])][getEdge(resistor.attributes['net_from'])] = reversed_valueToAssign
+
+capactors = input.getElementsByTagName('capactor')
+for capac in capactors:
+    currentValue = weights[getEdge(capac.attributes['net_from'])][getEdge(capac.attributes['net_to'])]
+    reversed_currentValue = weights[getEdge(capac.attributes['net_to'])][getEdge(capac.attributes['net_from'])]
+    value = float(capac.attributes['resistance'].value)
+    valueToAssign = value if currentValue == 0 else calcResistance(currentValue, value)
+    reversed_valueToAssign = value if reversed_currentValue == 0 else calcResistance(reversed_currentValue, value)
+    weights[getEdge(capac.attributes['net_from'])][getEdge(capac.attributes['net_to'])] = valueToAssign
+    weights[getEdge(capac.attributes['net_to'])][getEdge(capac.attributes['net_from'])] = reversed_valueToAssign
+
+diods = input.getElementsByTagName('diode')
+for diod in diods:
+    currentValue = weights[getEdge(diod.attributes['net_from'])][getEdge(diod.attributes['net_to'])]
+    reversed_currentValue = weights[getEdge(diod.attributes['net_to'])][getEdge(diod.attributes['net_from'])]
+    value = float(diod.attributes['resistance'].value)
+    valueToAssign = value if currentValue == 0 else calcResistance(currentValue, value)
+    reversed_valueToAssign = value if reversed_currentValue == 0 else calcResistance(reversed_currentValue, value)
+    weights[getEdge(diod.attributes['net_from'])][getEdge(diod.attributes['net_to'])] = valueToAssign
+    weights[getEdge(diod.attributes['net_to'])][getEdge(diod.attributes['net_from'])] = reversed_valueToAssign
+
+
+print(weights)
