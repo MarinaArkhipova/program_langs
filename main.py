4e55f891583ea6448b636b92ea69c5989bfd9ef1
root <root@linuxmint.linuxmint-domain>
2015-12-05 00:12:25 +0400

    mistake fixed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from xml.dom.minidom import parse
+import matrixops
 
 def getEdge(value):
     return int(value.value) - 1
@@ -11,7 +12,7 @@ input = parse("input.xml")
 weights = []
 
 ids = input.getElementsByTagName('net')
-for i in xrange(len(ids)):
+for i in range(len(ids)):
     weights.append([0]*len(ids))
 
 resists = input.getElementsByTagName('resistor')
@@ -39,10 +40,11 @@ for diod in diods:
     currentValue = weights[getEdge(diod.attributes['net_from'])][getEdge(diod.attributes['net_to'])]
     reversed_currentValue = weights[getEdge(diod.attributes['net_to'])][getEdge(diod.attributes['net_from'])]
     value = float(diod.attributes['resistance'].value)
+    reversed_value = float(diod.attributes['reverse_resistance'].value)
     valueToAssign = value if currentValue == 0 else calcResistance(currentValue, value)
-    reversed_valueToAssign = value if reversed_currentValue == 0 else calcResistance(reversed_currentValue, value)
+    reversed_valueToAssign = reversed_value if reversed_currentValue == 0 else calcResistance(reversed_currentValue, reversed_value)
     weights[getEdge(diod.attributes['net_from'])][getEdge(diod.attributes['net_to'])] = valueToAssign
     weights[getEdge(diod.attributes['net_to'])][getEdge(diod.attributes['net_from'])] = reversed_valueToAssign
 
-
-print(weights)
+result = matrixops.applyFloydWarshal(weights)
+print(result)
